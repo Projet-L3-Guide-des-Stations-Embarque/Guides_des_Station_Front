2e646d367f57b5c0af877687cecc82dc200442f7
AndrienMont <92160718+AndrienMont@users.noreply.github.com>
2023-04-04 10:36:41 +0200

    Base pour commencer

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+function About()
+{
+    return(
+        <>
+         <p>salut</p>
+         <Link to="/">Home</Link>
+        </>
+    )
+}
+
+export default About;
