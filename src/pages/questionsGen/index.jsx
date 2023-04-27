8afcbd279bf5632e7d122fa6cbf82babd06bca23
Khmarigou <luca.plc@outlook.fr>
2023-04-27 14:34:10 +0200

    Reagencement du navbar

--- src/pages/questionsGen/index.jsx
+++ src/pages/questionsGen/index.jsx
@@ -54,10 +54,10 @@ function Questions()
         <>
             <div className="topnav">
                 <Link to="/">Accueil</Link>
-                <Link to="/apropos">A propos</Link>
                 <Link to="/questions" className="active">Générer les questions</Link>
                 <Link to="/stations">Générer les pages des stations</Link>
                 <Link to="/tutoriel">Tutoriel pour KML</Link>
+                <Link to="/apropos">A propos</Link>
             </div>
             <p>Page pour générer les questions</p>
             <div className="App">
