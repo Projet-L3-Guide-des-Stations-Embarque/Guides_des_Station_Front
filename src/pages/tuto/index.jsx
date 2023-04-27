6cc3aefbbae714b1a2a7a7bc5002a95e205ca477
MaxBuster <maxbuster380@gmail.com>
2023-04-27 16:03:41 +0200

    Page tutoriel insérée dans le bon fichier

--- src/pages/tuto/index.jsx
+++ src/pages/tuto/index.jsx
@@ -4,7 +4,105 @@ function Tuto()
 {
     return(
         <>
-         <p>Tutoriel pour fichier KML ici</p>
+         <body>
+    <center><h1>Tutoriel de création de zones géographiques pour un guide forestier digital</h1></center>
+
+    <ul>
+        <li>1. Introduction</li>
+        <li>2. Préparation</li>
+        <li>3. Création</li>
+        <li>4. Exportation</li>
+    </ul>
+
+    <h2>1. Introduction</h2>
+
+    <p>Certains guides forestiers incluent une carte, permettant d'adapter des jugements et conclusions en fonction du terrain local. Ce document montre comment réaliser un fichier pouvant être lu par l'application Guide des Stations.</p>
+
+    <center>
+        <div style="display: flex;">
+            <img src="./carteGuide.png" style="width:20vw;">
+            <img src="./carteGoogleEarth.png" style="width:20vw;">
+        </div>
+    </center>
+
+    <h2>2. Préparation</h2>
+
+    <p>
+        L’application utilise un fichier « KML », soi un fichier avec l’extension « .kml ». 
+    </p>
+
+    <h2>3. Création</h2>
+
+    <h3>Initialisation</h3>
+
+    <p>Ouvrez Google Earth.</p>
+
+    <center><img src="./googleEarth.png" style="width:40vw;"></center>
+
+    <p>Sélectionnez « Projets ».</p>
+
+    <center><img src="./projets.png" style="width:40vw;"></center>
+
+    <p>Sélectionnez « Créer ».</p>
+
+    <center><img src="./creerProjet.png" style="width:40vw;"></center>
+
+    <p>Sélectionnez « Créer un fichier KML ».</p>
+
+    <center><img src="./fichierKML.png" style="width:40vw;"></center>
+
+    <h3>Ajouter une zone</h3>
+
+    <p>Dans le nouveau projet, sélectionnez « Nouvel élément ».</p>
+
+    <center><img src="./nouveauSecteur.png" style="width:40vw;"></center>
+
+    <p>Sélectionnez « Tracer une ligne ou une forme ».</p>
+
+    <center><img src="./tracerUneLigneOuUneForme.png" style="width:30vw;"></center>
+
+    <p>Cliquez sur la carte pour commencer votre tracé. Cliquez à nouveau pour créer un nouveau trait.</p>  
+
+    <center><img src="./debutLigne.png" style="width:40vw;"></center>
+
+    <p>Pour finir la zone, créez un nouveau tracé sur le point d’origine.</p>
+
+    <center>
+        <img src="./finLigne.png" style="width:40vw;">
+        <img src="./traceComplet.png" style="width:40vw;">
+    </center>
+
+    <h3>Nommer la nouvelle zone</h3>
+
+    <p>Le nom que vous donnez au secteur est important car il sera utilisé directement par l’outil pour l’identifier.
+Une zone est associée à un secteur (par exemple « secteur climatique »). Si ce secteur est identifié par un certain nombre, entrez ce nombre au début du nom de la zone.
+    </p>
+
+    
+    <center><img src="./triangleGuide.png" style="width:10vw;"></center>
+    <center><img src="./nomSecteur.png" style="width:30vw;"></center>
+    
+
+    <p>
+        <b>Attention</b>    L’application regardera les premiers chiffres du nom de la zone pour savoir à quel secteur elle est associée. Tant qu’il y a une séparation entre la fin du numéro de secteur et le reste du nom, tout est bon (voir exemples). Le reste du nom peut être ce que vous souhaitez.
+    </p>
+
+    <center><img src="./tableau.png" style="width:20vw;"></center>
+
+    <h2>4. Exportation</h2>
+
+    <p>Pour récupérer le fichier KML, sélectionnez « Autres actions ».</p>
+
+    <center><img src="./aExporter.png" style="width:50vw;"></center>
+
+    <p>Sélectionnez « Exporter en tant que fichier KML ».</p>
+
+    <center><img src="./exporterKml.png" style="width:30vw;"></center>
+
+    <p>Vous récupérez alors le fichier.</p>
+
+    <center><img src="./telechargement.png" style="width:40vw;"></center>
+</body>
          <Link to="/">Home</Link>
         </>
     )
