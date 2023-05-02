a48cc2e3a91c2c82929653eb04ab07a2a2a827b3
theoconnetable <56427602+theoconnetable@users.noreply.github.com>
2023-05-02 10:32:55 +0200

    Enregistrement en JSON d'une GE uniquement

--- src/pages/geGen/index.jsx
+++ src/pages/geGen/index.jsx
@@ -6,31 +6,41 @@ function Ge()
     const [idSuivant, setIdSuivant] = useState(1)
 
     const [inputFields, setInputFields] = useState([
-        { id:'0', image: '', descriptionfr:'', descriptionlat: ''}
+        { idPlante:'0', image: '', descriptionfr:'', descriptionlat: ''}
         //GERER PLUSIEUR + NOM GE + Image
+        /*
+        "GE":"7",
+        "titre":"Plantes des milieux très frais (hygroclines) • GE.7",
+        "images":[],
+        "titre_image":[],
+        "sous_titre_image":[]*/
     ])
     
     const handleFormChange = (index, event) => {
         let data = [...inputFields];
-        if (data[index]["id"] == "") {
-            data[index]["id"] = String(index);
+        if (data[index]["idPlante"] == "") {
+            data[index]["idPlante"] = String(index);
         }
-        data[index][event.target.name] = event.target.value;
-        setInputFields(data);
+        if(event.target.name === "image"){
+            //traitement sur image
+            // data[index][event.target.name] = getEmergencyFoundImg(event.target.value);
+            let base64string = "";
+            const files = event.target.files;
+            const file = files[0];
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () =>{
+                base64string = reader.result;
+                data[index][event.target.name] = base64string;
+            }
+        }else{
+            data[index][event.target.name] = event.target.value;
+            setInputFields(data);
+        }
     }
 
-    /*const handleCheckBoxChange = (index, event) => {
-        let data = [...inputFields];
-        data[index][event.target.name] = event.target.checked;
-        setInputFields(data);
-        if (event.target.checked) {
-            data[index]["idoui"] = "";
-            data[index]["idnon"] = "";
-        }
-    }*/
-
-    const addFields = () => {
-        let newfield = {id:String(idSuivant), image: '', descriptionfr:'', descriptionlat: ''}
+    const addPlante = () => {
+        let newfield = {idPlante:String(idSuivant), image: '', descriptionfr:'', descriptionlat: ''}
         setIdSuivant(idSuivant + 1)
         setInputFields([...inputFields, newfield])
     }
@@ -38,8 +48,33 @@ function Ge()
     const submit = (e) => {
         e.preventDefault();
         console.log(inputFields)
-        const fileData = JSON.stringify(inputFields);
-        const blob = new Blob([fileData], { type: "text/plain" });
+
+        var strfile = '[\n';
+        strfile = strfile + "{\n";
+        strfile = strfile + "\"GE\":\"1\",\n";
+        strfile = strfile + "\"titre\":\"LE TITRE DE LA GE\",\n";
+        
+        strfile = strfile + "\"images\":[\n";
+        for (var i = 0; i < inputFields.length; i++){
+            strfile = strfile + "\"" + inputFields[i]["image"] + "\",\n";
+        }
+        strfile = strfile + "],\n";
+        
+        strfile = strfile + "\"titre_image\":[\n";
+        for (var i = 0; i < inputFields.length; i++){
+            strfile = strfile + "\"" + inputFields[i]["descriptionfr"] + "\",\n";
+        }
+        strfile = strfile + "],\n";
+
+        strfile = strfile + "\"sous_titre_image\":[\n";
+        for (var i = 0; i < inputFields.length; i++){
+            strfile = strfile + "\"" + inputFields[i]["descriptionlat"] + "\",\n";
+        }
+        strfile = strfile + "]\n";
+        strfile = strfile + "}\n";
+        strfile = strfile + "]\n";
+        //const fileData = JSON.stringify(inputFields);
+        const blob = new Blob([strfile], { type: "text/plain" });
         const url = URL.createObjectURL(blob);
         const link = document.createElement("a");
         link.download = "ge.json";
@@ -64,31 +99,27 @@ function Ge()
                 <Link to="/tutoriel">Tutoriel pour KML</Link>
                 <Link to="/apropos">A propos</Link>
             </div>
-            <h2 className="catchPhrase">Vous pouvez ici générer la clé de determination des stations à l'aide de questions.</h2>
+            <h2 className="catchPhrase">Vous pouvez ici générer les différents groupes écologiques présents dans le guide.</h2>
             <div className="App">
                 <form>
                     {inputFields.map((input, index) => {
                     return (
                         <div key={index} className='questionFormulaire'>
-                            <>{"Numéro du Groupe écologique : "}
+                            <>{"Image de la plante : "}
                                     <input 
-                                        type='text'
-                                        name='id'
+                                        type='file'
+                                        name='image'
                                         value={input.id}
                                         onChange={event => handleFormChange(index, event)}
                                     />
                                     <div className='formulairedeLaGE'>
                                         <div>
-                                            <label>Image de la plante : </label>
-                                            <input type="file" name="imageElement" placeholder="Image" multiple accept="image/*" onChange={event =>handleFormChange(index,event)}/>
-                                        </div>
-                                        <div>
                                             <label>Nom (français) : </label>
-                                            <input type='text' name="textFr" value={input.descriptionfr} onChange={event =>handleFormChange(index,event)}/>
+                                            <input type='text' name="descriptionfr" placeholder="Nom de la plante" value={input.descriptionfr} onChange={event =>handleFormChange(index,event)}/>
                                         </div>
                                         <div>
                                             <label>Nom (latin) : </label>
-                                            <input type='text' name="textLat" value={input.descriptionlat} onChange={event =>handleFormChange(index,event)}/>
+                                            <input type='text' name="descriptionlat" placeholder="Nom de la plante" value={input.descriptionlat} onChange={event =>handleFormChange(index,event)}/>
                                         </div>
                                     </div>
                                 </> 
@@ -99,7 +130,7 @@ function Ge()
                     })}
                 </form>
                 <div className='formulaireFin'>
-                    <button onClick={addFields}>Ajouter une plante au Ge</button>
+                    <button onClick={addPlante}>Ajouter une plante au Ge</button>
                     <button onClick={submit}>Envoyer</button>
                 </div>
             </div>
